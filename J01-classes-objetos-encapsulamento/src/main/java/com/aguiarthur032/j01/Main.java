49d013e4eb78fd20006e88cd99ad057f1dca72ed
Arthur Aguiar <aguiarthur.32@gmail.com>
2026-04-15 00:25:41 -0300

    fix: corrige do pom.xml

--- J01-classes-objetos-encapsulamento/src/main/java/com/aguiarthur032/j01/Main.java
+++ J01-classes-objetos-encapsulamento/src/main/java/com/aguiarthur032/j01/Main.java
@@ -2,5 +2,9 @@ package com.aguiarthur032.j01;
 
 public class Main {
 
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+    }
 }
                                                                                                                                                                                                     
--- J02-heranca-polimorfismo/src/main/java/com/aguiarthur032/j02/Main.java
+++ J02-heranca-polimorfismo/src/main/java/com/aguiarthur032/j02/Main.java
@@ -2,4 +2,7 @@ package com.aguiarthur032.j02;
 
 public class Main {
     
+    public static void main(String[] args){
+        System.out.println("Hello World 2!");
+    }
 }
